7e1f220adc7494d721464ecb6c26a6ab6d996c72
Victor <b25arbarov@gmail.com>
2025-11-09 12:22:28 +0300

    First commit: Add initial project structure with Maven configuration and Spring Boot setup

---
+++ nasd/src/main/java/com/rayyan/nasd/NasdApplication.java
@@ -0,0 +1,13 @@
+package com.rayyan.nasd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasdApplication.class, args);
+	}
+
+}
